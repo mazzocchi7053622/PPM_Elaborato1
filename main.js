594b7d231cd64d6650e4fd950f4401a54a292133
mazzocchi7053622 <167107766+mazzocchi7053622@users.noreply.github.com>
2024-05-20 17:28:42 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+import '../scss/styles.scss'
+import '@fortawesome/fontawesome-free/js/all.js';
+
+// Funzione per aprire o chiudere un elemento
+function toggleElement(elementId) {
+    var body = document.getElementById('page');
+    var element = document.getElementById(elementId);
+    if(element.style.display === 'block') {
+        element.style.display = 'none';
+        body.style.overflowY= 'auto';
+    } else {
+        element.style.display = 'block';
+        body.style.overflowY= 'hidden';
+    }
+}
+
+// Funzione per chiudere un elemento
+function closeElement(elementId) {
+    var element = document.getElementById(elementId);
+    if(element.style.display === 'block') {
+        element.style.display = 'none';
+    }
+}
+
+document.getElementById('selects-button').addEventListener('click', function() {
+    // Chiudi la barra di ricerca se è aperta
+    closeElement('search');
+    // Apri o chiudi il menu a tendina
+    toggleElement('arrow');
+    toggleElement('dropdown-menu');
+});
+
+document.getElementById('search-button').addEventListener('click', function() {
+    // Chiudi il menu a tendina se è aperto
+    closeElement('arrow');
+    closeElement('dropdown-menu');
+    // Apri o chiudi la barra di ricerca
+    toggleElement('search');
+});
